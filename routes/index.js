09d23b4b92cf0ad74b538499d74571fc43c63087
Ashton Pieterse <ashton@fusebox.co.za>
2023-02-25 17:02:27 +0200

    Added lesson 2 work

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const routes = require('express').Router();
 routes.get('/', (req, res) => {
   res.send('Cailyn Pieterse');
 });
+routes.use('/contacts', require('./contacts'))
 
 module.exports = routes;
